486a8250e9c623dfc6a80553d2b112255ff91aa1
baylordelrey <73312444+baylordelrey@users.noreply.github.com>
2021-05-25 13:02:56 -1000

    Add files via upload

---
+++ Circle_Rove.py
@@ -0,0 +1,32 @@
+from roboclaw import Roboclaw
+from time import sleep
+
+if __name__ == "__main__":
+    
+    address1 = 0x80
+    address2 = 0x81
+    address3 = 0x82
+    roboclaw = Roboclaw("/dev/ttyS0", 38400)
+    roboclaw.Open()
+    
+    while True:
+        roboclaw.ForwardM1(address1,64)
+        roboclaw.ForwardM2(address1, 64)
+        roboclaw.ForwardM1(address2,64)
+        roboclaw.ForwardM2(address2, 64)
+        roboclaw.ForwardM1(address3,64)
+        roboclaw.ForwardM2(address3, 64)
+        sleep(4)
+        roboclaw.ForwardM1(address1,0)
+        roboclaw.ForwardM2(address1,0)
+        roboclaw.ForwardM1(address2,0)
+        roboclaw.ForwardM2(address2,0)
+        roboclaw.ForwardM1(address3,0)
+        roboclaw.ForwardM2(address3,0)
+        sleep(2)
+        
+    
+   
+  
+    
+    
